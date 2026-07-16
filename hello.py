e0c34d8bc6ffb0b03d9da8a8269c2b295ab53160
Sneha7991 <Sneha.a9820@gmail.com>
2026-07-16 21:32:38 +0530

    Add files via upload
    Basic python program

---
+++ hello.py
@@ -0,0 +1,10 @@
+# My First Python Program 
+print ("Welcome to MetricMind")
+name =  input("Enter your name: ")
+age = int(input("Enter your age: "))
+print( "Hello,", name)
+print( "you are " + str(age) + " years old.")
+def greet():
+ 
+  print("Happy Learning python!")
+greet()
